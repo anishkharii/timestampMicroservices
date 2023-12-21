c72a09d1f7ae7d879629782131b045359ed26c10
Anish Khari <anishkhari91@gmail.com>
2023-12-21 13:44:06 +0530

    changing internal working

--- index.js
+++ index.js
@@ -3,16 +3,6 @@ const cors = require("cors");
 const app = express();
 app.use('/public',express.static('public'));
 app.use(cors({optionsSuccessStatus:200}));
-const options={
-    weekday:'short',
-    day:'2-digit',
-    month:'short',
-    year:'numeric',
-    hour:'2-digit',
-    minute:'2-digit',
-    second:'2-digit',
-    timeZoneName:'short'
-};
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/views/index.html');
@@ -24,26 +14,23 @@ app.get('/api',(req,res)=>{
     res.json({unix,utc});
 })
 app.get('/api/:date',(req,res)=>{
-    const dateParam = req.params.date;
-
-    const yyyyFormatCheck = /^\d{4}-\d{2}-\d{2}$/;
-    const unixFormatCheck = /^\d+$/;
-
-    if(yyyyFormatCheck.test(dateParam)){
-        let date = new Date(dateParam);
-        let unix =date.getTime();
-        let utc = date.toUTCString();
-        res.json({unix,utc});
-    }
-    else if(unixFormatCheck.test(dateParam)){
-        let date = new Date(parseInt(dateParam));
-        let unix =date().getTime();
-        let utc = date.toUTCString();
-        res.json({unix,utc});
-    }
-    else{
-        res.json({error:"Invalid Date"});
-    }
+  const dateParam = req.params.date;
+  let date ;
+  if(!dateParam){
+    date = new Date();
+  }
+  else{
+    let checkUnix = dateParam*1;
+    date = isNaN(checkUnix)?new Date(dateParam): new Date(checkUnix);
+  }
+  if(date.toString()==='Invalid Date'){
+    res.json({error:date.toString()});
+  }
+  else{
+    let unix = date.getTime();
+    let utc= date.toUTCString();
+    res.json({unix,utc});
+  }
 })
 app.listen(3000,()=>{
     console.log("Connected");
